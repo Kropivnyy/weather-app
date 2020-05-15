739af64f3dd405cd9a11fec7f886ba4d78fcd700
Svetlana Zlydneva <svetlanaderiglazova87@gmail.com>
2020-05-15 15:26:44 +0300

    added refs.js file, registered links for the search section in it, fixed arrow left

---
+++ src/js/refs.js
@@ -0,0 +1,8 @@
+export const refs = {
+  searchForm: document.querySelector('.search__form'),
+  formInput: document.querySelector('.form__input'),
+  formAddToFavorites: document.querySelector('.form__add-to-favorites'),
+  favoritesRemove: document.querySelector('.favorites__remove'),
+  formIconStar: document.querySelector('.form__icon-star'),
+  favoritesArrowLeft: document.querySelector('.favorites__arrow-left'),
+};
