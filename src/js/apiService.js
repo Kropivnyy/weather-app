84eb2e3d0708557f1c5a81f4bd3dca0caac47a14
Aleksandr Kropivnyy <kropivnyy@gmail.com>
2020-05-15 18:52:50 +0300

    some structure fix

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -5,30 +5,15 @@ const apiKey = 'c112c800340c3f1ee2fad83b32fe690c';
 
 export default {
   searchQuery: '',
-  pageNumber: 1,
-  result: {},
-  date: 0,
   fetchImages: async function () {
     try {
       const { data } = await axios.get(
         `weather?q=${this.query}&units=metric&appid=${apiKey}`,
       );
-      this.result = { ...data };
-      this.date = data.dt + data.timezone;
-      // console.log(`this.date: ${this.date}`);
       return data;
     } catch (error) {
       console.log(error);
     }
-  },
-  resetResult() {
-    this.result = [];
-  },
-  resetPage() {
-    this.pageNumber = 1;
-  },
-  pageIncrement() {
-    this.pageNumber += 1;
   },
   get query() {
     return this.searchQuery;
