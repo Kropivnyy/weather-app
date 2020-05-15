1bd2d1da5b96d1354e1dc7b39cad85f2c6ca7ed0
Aleksandr Kropivnyy <kropivnyy@gmail.com>
2020-05-15 19:10:20 +0300

    structure small fix

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import './styles.scss';
-import debounce from 'lodash.debounce';
 import refs from './js/refs';
 import apiService from './js/apiService';
 import getCurrentTime from './js/get-current-time';
 
-refs.input.addEventListener(
-  'input',
-  debounce(async () => {
-    apiService.query = refs.input.value.toLowerCase();
-    const data = await apiService.fetchImages();
-    console.log(data);
-    console.log(getCurrentTime(data));
-  }, 500),
-);
+refs.searchForm.addEventListener('submit', async event => {
+  event.preventDefault();
+  apiService.query = refs.formInput.value.toLowerCase();
+  const data = await apiService.fetchImages();
+  console.log(data);
+  console.log(getCurrentTime(data));
+});
