e76763c023711ef132c94c4046f8729139c1f860
Aleksandr Kropivnyy <kropivnyy@gmail.com>
2020-05-15 18:32:13 +0300

    changed output format

--- src/js/get-current-time.js
+++ src/js/get-current-time.js
@@ -3,12 +3,16 @@ import getTimezoneTime from './get-timezone-time';
 export default function ({ timezone }) {
   const time = new Date();
   getTimezoneTime(time, timezone);
-  const month = time.getMonth();
-  const day = time.getDay();
-  const date = time.getDate();
-  const hours = time.getHours();
-  const mins = time.getMinutes();
-  const secs = time.getSeconds();
+  const locales = 'en-US';
+  const month = time.toLocaleString(locales, { month: 'long' });
+  const day = time.toLocaleString(locales, { weekday: 'short' });
+  const date = time.toLocaleString(locales, { day: 'numeric' });
+  const hours = time.toLocaleString(locales, {
+    hour12: false,
+    hour: '2-digit',
+  });
+  const mins = time.toLocaleString(locales, { minute: '2-digit' });
+  const secs = time.toLocaleString(locales, { second: '2-digit' });
   return {
     total: time.getTime(),
     month,
