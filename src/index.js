fe832d4b15be878004606f655da9a2319f03d7b4
Aleksandr Kropivnyy <kropivnyy@gmail.com>
2020-05-15 23:45:24 +0300

    added widget template

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import './styles.scss';
 import refs from './js/refs';
 import apiService from './js/apiService';
-import getCurrentTime from './js/get-current-time';
+// import getCurrentTime from './js/get-current-time';
+import widgetTemplate from './templates/current-weather.hbs';
 
 refs.searchForm.addEventListener('submit', async event => {
   event.preventDefault();
   apiService.query = refs.formInput.value.toLowerCase();
-  const data = await apiService.fetchImages();
-  console.log(data);
-  console.log(getCurrentTime(data));
+  await apiService.fetchCurrentWeather();
+  console.log(apiService.apiResponse);
+  const markup = widgetTemplate(apiService.apiResponse);
+  refs.currentWeather.innerHTML = markup;
 });
