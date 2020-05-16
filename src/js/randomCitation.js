7b279966f19890d85f61cd59340f02f62775d191
LeYuliia <legeza.y@gmail.com>
2020-05-16 11:59:01 +0300

    styles + correct random citations

--- src/js/randomCitation.js
+++ src/js/randomCitation.js
@@ -3,16 +3,15 @@ import citations from './json/citation.json';
 const random = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
-
 const randomCitation = citations[random(0, citations.length - 1)];
-const autorName = Object.keys(randomCitation);
+const authorName = Object.keys(randomCitation);
 const citationText = Object.values(randomCitation);
 
 window.addEventListener('load', () => {
   document
-    .querySelector('.citation-text')
-    .insertAdjacentText('afterbegin', `${citationText}`);
+    .querySelector('.citation__text')
+    .insertAdjacentText('afterbegin', citationText);
   document
-    .querySelector('.citation-autor')
-    .insertAdjacentText('afterbegin', `${autorName}`);
+    .querySelector('.citation__author')
+    .insertAdjacentText('afterbegin', authorName);
 });
