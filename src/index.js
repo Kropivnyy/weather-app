ea4bb416926d3e2bdc24cdd73630c65284448f3d
Aleksandr Kropivnyy <kropivnyy@gmail.com>
2020-05-17 15:59:19 +0300

    begin to create timer

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import apiService from './js/apiService';
 import getCurrentTime from './js/get-current-time';
 import dateTemplate from './templates/current-date.hbs';
 import widgetTemplate from './templates/current-weather.hbs';
-
+import { createClock, timer } from './js/timerService';
+createClock(timer.selector, timer.targetDate);
 refs.searchForm.addEventListener('submit', async event => {
   event.preventDefault();
   apiService.query = refs.formInput.value.toLowerCase();
