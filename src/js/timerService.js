1eb172042526e51fee52f9694bd2adf29621957a
Kropivnyy <55694059+Kropivnyy@users.noreply.github.com>
2020-05-17 18:23:30 +0300

    added timer

---
+++ src/js/timerService.js
@@ -0,0 +1,32 @@
+import apiService from './apiService';
+import getCurrentTime from './get-current-time';
+
+export default function (clock) {
+  const idRef = document.querySelector(clock);
+  const dateRef = idRef.querySelector('[data-value="date"]');
+  const suffixRef = idRef.querySelector('[data-value="suffix"]');
+  const dayRef = idRef.querySelector('[data-value="day"]');
+  const monthRef = idRef.querySelector('[data-value="month"]');
+  const hoursRef = idRef.querySelector('[data-value="hours"]');
+
+  function updateTime() {
+    const t = getCurrentTime(apiService.apiResponse.timezone);
+    dateRef.textContent = t.date;
+    if (t.date === '1' || t.date === '21' || t.date === '31') {
+      suffixRef.textContent = 'st';
+    } else if (t.date === '2' || t.date === '22') {
+      suffixRef.textContent = 'nd';
+    } else if (t.date === '3' || t.date === '23') {
+      suffixRef.textContent = 'rd';
+    } else {
+      suffixRef.textContent = 'th';
+    }
+    dayRef.textContent = t.day;
+    monthRef.textContent = t.month;
+    hoursRef.textContent = `${t.hours < 10 ? '0' + t.hours : t.hours}:${
+      t.mins < 10 ? '0' + t.mins : t.mins
+    }:${t.secs < 10 ? '0' + t.secs : t.secs}`;
+  }
+  updateTime();
+  const timerId = setInterval(updateTime, 1000);
+}
